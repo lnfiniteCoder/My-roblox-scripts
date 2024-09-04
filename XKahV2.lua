46289916cec4a133d96e71134d1fec6dc28c7ca3
lnfiniteCoder <144736019+lnfiniteCoder@users.noreply.github.com>
2024-09-04 16:33:01 +0200

    Update XKahV2.lua

--- XKahV2.lua
+++ XKahV2.lua
@@ -1,6 +1,6 @@
 -- NOTE : Some parts of scripts are not exactly scripted, and some are created by others(Like dizzy made admin pad grab and anti brick kill), other parts of scripts are either scripted or copied from my previous script(XKah(XK or XKV1))
 
-wait(1)
+game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
 
 xowner = "Ih0temyIife" or "Sans_fromlb"
 
@@ -19,6 +19,7 @@ local lp = game.Players.LocalPlayer
 local char = lp.Character
 local humanoid = char:WaitForChild("Humanoid")
 local humanoid2 = char.HumanoidRootPart
+local backpack = lp.Backpack
 
 local getplrs = game.Players:GetPlayers()
 local lighting = game:GetService("Lighting")
@@ -308,12 +309,11 @@ v.Click:FireServer(game.Players[gplr].Character:GetPivot().Position)
           ct("spam equipall")
         elseif args1 == "crashed" then
           wait()
-          for i = 1,5 do
-            ct("gear all 94794847")
-          end
+          ct("gear all 94794847")   
+          backpack:WaitForChild("VampireVanquisher")
+          backpack.VampireVanquisher.Parent = char
+          backpack.VampireVanquisher:Activate()
           wait()
-          ct("equipall")
-          wait(0.1)
           for i = 1,100 do
             ct("unsize all all all")
 					end
